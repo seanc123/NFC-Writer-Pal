4729a4810457af252e3711a859a5f8472bac15a9
seanc <sean.p.collins@mycit.ie>
2017-05-01 12:50:14 +0100

    Removed settings option

--- NFCWriterPal/app/src/main/java/com/apps/seanc/nfcwriterpal/ActivityHandlers/FormatActivity.java
+++ NFCWriterPal/app/src/main/java/com/apps/seanc/nfcwriterpal/ActivityHandlers/FormatActivity.java
@@ -166,7 +166,7 @@ public class FormatActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.format, menu);
+        //getMenuInflater().inflate(R.menu.format, menu);
         return true;
     }
 
